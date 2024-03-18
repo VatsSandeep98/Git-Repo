f417b3fabc10c9a755ae6728a91516aead7bc189
vats.sandeep98 <vats.sandeep98@gmail.com>
2024-03-19 00:59:18 +0530

    new

---
+++ RG/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "sharma-rg"
+#     storage_account_name = "sandeepstg"
+#     container_name       = "sandeep123"
+#     key                  = "rgterraform.tfstate"
+#   }
+# }
+
+
+provider "azurerm" {
+ features {
+   
+ }
+}
